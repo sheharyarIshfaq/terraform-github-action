bbafbd3129cae2c49f48c7c5cf5c7f810db1e8cd
Sheharyar <83644026+sheharyarIshfaq@users.noreply.github.com>
2023-04-29 22:05:57 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,37 @@ provider "aws" {
   region = "us-east-1" # define region as per your account
 }
 
+resource "aws_instance" "test" {
+  ami                         = "ami-052efd3df9dad4825"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  security_groups             = [aws_security_group.test.name]
+  tags = {
+    Name = var.name
+  }
+}
+################### SECURITY GROUP ##########################
+resource "aws_security_group" "test" {
+  name        = var.name
+  description = "Allow TLS inbound traffic"
+  ingress {
+    description = "allow access to web"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = var.name
+  }
+}
+
 resource "aws_s3_bucket" "terraform-test-bucket-sheharyar" {
   bucket = "demo-github-action-tf-medium-sheharyar"
 
